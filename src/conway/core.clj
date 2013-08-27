430e297eeed5555aed61c3b163a9dc2e1ed6d910
James Henderson <james@pcr.me.uk>
2013-08-27 20:22:04 +0100

    Written create-grid and step fns

--- src/conway/core.clj
+++ src/conway/core.clj
@@ -1,6 +1,36 @@
 (ns conway.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn live?
+  [n]
+  (zero? (rand-int n)))
+
+(defn create-map
+  [n f]
+  (->> (for [i (range n)
+             j (range n)]
+         [[i j] (f n)])
+       (into {})))
+
+
+(defn neighbours
+  [grid x y]
+  (for [i (range -1 1)
+        j (range -1 1)
+        :when (not (and (zero? i) (zero? j)))]
+    (grid [(+ x i) (+ y j)])))
+
+(defn count-neighbours [neighbours]
+  (count (filter identity neighbours)))
+
+(def alive-next-step?
+  (comp boolean #{2 3}))
+
+(defn step
+  [grid]
+  (->> (for [[x y] grid]
+         [[x y] (->> (neighbours grid x y)
+                     count-neighbours
+                     alive-next-step?)])
+       (into {})))
+
+
